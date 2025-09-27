4cda322406c7f52f18fa72b0e787ddf00b51eec2
= <korniykom@gmail.com>
2025-09-27 19:14:49 +0000

    feat: create referesh token storage system

---
+++ chat_backend/user/src/main/kotlin/com/korniykom/chat_backend/user/infra/database/repositories/RefreshTokenRepository.kt
@@ -0,0 +1,11 @@
+package com.korniykom.chat_backend.user.infra.database.repositories
+
+import com.korniykom.chat_backend.user.domain.model.UserId
+import com.korniykom.chat_backend.user.infra.database.entity.RefreshTokenEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface RefreshTokenRepository: JpaRepository<RefreshTokenRepository, Long> {
+    fun findByUserIdAndHashedToken(userId: UserId, hashedToken: String): RefreshTokenEntity?
+    fun deleteByUserIdAndHashedToken(userId: UserId, hashedToken: String)
+    fun deleteByUserId(userId: UserId)
+}
